e126037f745fa6c6a3c2707cf736df06903acf39
ndizeye alain <alainndizeye11@gmail.com>
2025-10-04 21:41:00 +0200

    added  backend/.eslintrc.js

---
+++ backend/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'prettier/prettier': 'error'
+  },
+  ignorePatterns: ['node_modules/', 'dist/']
+};
